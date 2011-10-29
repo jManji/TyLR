a5e9b8d5584f9f6619e4163589a161cab6050337
Stathis Moraitidis <sta7mor@yahoo.gr>
2011-10-29 16:41:59 +0100

    Will abandon the sensor approach.

--- src/gr/tylr/entity/Hero.java
+++ src/gr/tylr/entity/Hero.java
@@ -7,6 +7,7 @@ import gr.tylr.util.Consts.Button;
 import gr.tylr.util.Consts.HeroState;
 import static gr.tylr.util.Consts.HeroState.NORMAL;
 import static gr.tylr.util.Consts.HeroState.SPAWN;
+import gr.tylr.util.Util;
 import org.jbox2d.callbacks.QueryCallback;
 import org.jbox2d.collision.AABB;
 import org.jbox2d.collision.shapes.PolygonShape;
@@ -14,6 +15,7 @@ import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Fixture;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
+import org.newdawn.slick.geom.Polygon;
 
 /**
  *
@@ -25,7 +27,7 @@ public class Hero extends DynamicEntity {
     private final static float JUMP_FORCE_DOWN = 20.0f;
     private final static float MOVEMENT_FORCE = 30.0f;
     private final static int MAX_JUMP_TIMER = 400;
-	private final static float ON_GROUND_OFFSET = 0.1f;
+	private final static float ON_GROUND_OFFSET = 0.46f;
     public final int SPAWN_RANGE = 4; 
     private long jumpTimer;
     private TileSpawner tileSpawner;
@@ -35,6 +37,7 @@ public class Hero extends DynamicEntity {
 	private AABB aabb;
 	onGroundCallback onGroundCallback;
 	StaticEntity s;
+	Polygon testPolygon;
     
     public Hero(Vec2 position, Vec2 size, String name) {
         super(position, size, name, Sprite.MEGAMAN);
@@ -43,20 +46,43 @@ public class Hero extends DynamicEntity {
 		
 		aabb = new AABB();
 		onGroundCallback = new onGroundCallback();
+		// Shape		
+		Vec2 testSize = new Vec2(0.4f*Consts.PTM_SCALE, 
+						         0.4f*Consts.PTM_SCALE);
+        float square[] = { -testSize.x, -testSize.y,
+                            testSize.x, -testSize.y,
+                            testSize.x,  testSize.y,
+                           -testSize.x,  testSize.y};
+
+        testPolygon = new Polygon(new float[]{ square[0], square[1],
+                                           square[2], square[3],
+                                           square[4], square[5],
+                                           square[6], square[7] });		
+    }
+	
+	public Hero(Vec2 position, String name) {
+        this(position, 
+		     new Vec2(Sprite.MEGAMAN.getWidth(), Sprite.MEGAMAN.getHeight()), 
+			 name);
     }
     
     public void update(final int delta) {
+
+		
 		
 //		System.out.println(isOnGround());
         switch (state) {
             case NORMAL: {
-                
-                super.update(delta);
-        
+                                        										
                 jump(delta);
-
+								
                 move();
                 
+				super.update(delta);
+				
+				testPolygon.setX(getWorldPosition().x + size.x/4);
+				testPolygon.setY(600 - (getWorldPosition().y - ON_GROUND_OFFSET*Consts.PTM_SCALE));
+				
                 break;
             }
             case SPAWN: {
@@ -69,7 +95,7 @@ public class Hero extends DynamicEntity {
                 // Do nothing again
             }
         }
-
+		
         spawnTiles();
     }
     
@@ -126,57 +152,72 @@ public class Hero extends DynamicEntity {
         state = NORMAL;
     }
 	
-//	public void render(Graphics g) {		
-//		sprite.draw(polygon.getX(), polygon.getY());
-//		
+	public void render(Graphics g) {		
+		sprite.draw(polygon.getX(), polygon.getY());
+		
 //		s = new StaticEntity(getWorldPosition(),
 //				new Vec2(ON_GROUND_OFFSET*Consts.PTM_SCALE, ON_GROUND_OFFSET*Consts.PTM_SCALE), "TEST");
-//		g.draw(s.getPolygon());
-//	}
+			
+		g.draw(testPolygon);
+	}
 	
 	
 	public class onGroundCallback implements QueryCallback {
 
 		public boolean reportFixture(Fixture fixture) {		
 			
+			GameplayState.getWorld().clearForces();
 			AbstractPhysicsEntity entity = 
 					(AbstractPhysicsEntity)(fixture.getBody().getUserData());
 			
-			System.out.println(entity.getName());
-	
-//			if (entity.getName().equals("MAP_TILE_1_11")) {
-//			System.out.println("MAP_TILE_1_11: " + (((size.y/2)/Consts.PTM_SCALE) + (entity.bodyDef.position.y + size.y/2)/Consts.PTM_SCALE));
-//			}
-//			if (entity.getName().equals("HERO")) {
-//				System.out.println("HERO: " + ((-(size.y/2)/Consts.PTM_SCALE) + (entity.bodyDef.position.y + size.y/2)/Consts.PTM_SCALE));
-//			}
-			
+			System.out.println(entity.getName());		
 			
 			if (entity.getName().equals("HERO")) {
 				isOnAir = true;
-//				System.out.println("HERO");
+				
+				System.out.println(600 - polygon.getMaxY());
+				System.out.println(aabb.upperBound.y*Consts.PTM_SCALE);
+				System.out.println(aabb.lowerBound.y*Consts.PTM_SCALE);
+				System.out.println("=======");
+				
+////				System.out.println("HERO");
+//				
+//				System.out.println(body.getPosition().y - (size.y/2)/Consts.PTM_SCALE );
+//				System.out.println(aabb.upperBound);
+//				System.out.println(aabb.lowerBound);
 			} 
 
 			
 			// terminate query		
-			return false;
+			return true;
 		}		
 	} 
 	
 	public boolean isOnGround() {
 		
 		isOnAir = false;
+		
+		
+//		aabb.lowerBound.set(body.getPosition().x - ON_GROUND_OFFSET,
+//							(600 - polygon.getMaxY())/Consts.PTM_SCALE - 2*ON_GROUND_OFFSET);
+//		aabb.upperBound.set(body.getPosition().x + ON_GROUND_OFFSET, 
+//							(600 - polygon.getMaxY())/Consts.PTM_SCALE - ON_GROUND_OFFSET);
+				
+		
+		aabb.lowerBound.set(testPolygon.getMinX()/Consts.PTM_SCALE,
+							(600 - testPolygon.getMaxY())/Consts.PTM_SCALE);
+		
+		aabb.upperBound.set(testPolygon.getMaxX()/Consts.PTM_SCALE,
+							(600 - testPolygon.getMinY())/Consts.PTM_SCALE);		
+		
+		
+//		aabb.lowerBound.set(body.getPosition().x - ON_GROUND_OFFSET,
+//							body.getPosition().y - Consts.PTM_SCALE/(size.y) - ON_GROUND_OFFSET;
+//		aabb.upperBound.set(body.getPosition().x + ON_GROUND_OFFSET,
+//							body.getPosition().y - Consts.PTM_SCALE/(size.y) - ON_GROUND_OFFSET);
+		
+//		System.out.println(body.getPosition().y*Consts.PTM_SCALE - Consts.PTM_SCALE*Consts.PTM_SCALE/(size.y));
 
-		aabb.lowerBound.set(body.getPosition().x - ON_GROUND_OFFSET,							
-							body.getPosition().y - (size.y/2)/Consts.PTM_SCALE - 3*ON_GROUND_OFFSET);
-		aabb.upperBound.set(body.getPosition().x + ON_GROUND_OFFSET, 
-							body.getPosition().y - (size.y/2)/Consts.PTM_SCALE - 2*ON_GROUND_OFFSET);
-		
-//		System.out.println("onGround: " + (body.getPosition().y - (size.y/2)/Consts.PTM_SCALE - 2*ON_GROUND_OFFSET));				
-		
-//		System.out.println(aabb.upperBound);
-//		System.out.println(aabb.lowerBound);
-//		System.out.println("=======");
 //		PolygonShape shape = (PolygonShape)body.getFixtureList().m_shape;
 //		for (Vec2 v: shape.getVertices()) {
 //			System.out.println("v: " + v);
@@ -189,9 +230,8 @@ public class Hero extends DynamicEntity {
 //		System.out.println(aabb.getCenter());
 //		
 //		System.out.println("===== 0 =====");
-		
-		
-		GameplayState.getWorld().queryAABB(onGroundCallback, aabb);
+				
+		GameplayState.getWorld().queryAABB(onGroundCallback, aabb);		
 		
 		return !isOnAir;
 	}	
