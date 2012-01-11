9b560696f3d93878f4cd8922b1015fd4a26cd734
Stathis Moraitidis <sta7mor@yahoo.gr>
2012-01-11 20:50:10 +0000

    test git

--- src/gr/tylr/entity/AbstractPhysicsEntity.java
+++ src/gr/tylr/entity/AbstractPhysicsEntity.java
@@ -1,109 +1,240 @@
 package gr.tylr.entity;
 
 import gr.tylr.state.GameplayState;
+import gr.tylr.util.Consts;
+import gr.tylr.util.Consts.BodyShape;
+import gr.tylr.util.Consts.CollisionFilter;
 import static gr.tylr.util.Consts.PTM_SCALE;
+import gr.tylr.util.FilterData;
+import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.collision.shapes.Shape;
 import org.jbox2d.common.Vec2;
-import org.jbox2d.dynamics.Body;
-import org.jbox2d.dynamics.BodyDef;
-import org.jbox2d.dynamics.BodyType;
-import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.*;
 import org.newdawn.slick.Image;
 
 /**
  *
  * @author manji
  */
+        /************
+         * Graphics *
+         ************/
 public abstract class AbstractPhysicsEntity extends AbstractEntity {
-    
-    protected Body body;    
-    public BodyDef bodyDef;
-    protected PolygonShape box;
-    protected FixtureDef fixtureDef; 
-    
-    public AbstractPhysicsEntity(Vec2 position, 
-                                 Vec2 size,
-                                 boolean isDynamic,
-                                 String name) {
-        
-        super(position, size, name);
-        init(position, isDynamic);
-    }
-    
-    public AbstractPhysicsEntity(Vec2 position, 
-                                 Vec2 size,
-                                 boolean isDynamic,
-                                 String name,
-                                 Image sprite) {
-        super(position, size, name, sprite);
-        init(position, isDynamic);
-    }
-    
-    public AbstractPhysicsEntity(Vec2 position, 
-                                 boolean isDynamic,
-                                 String name,
-                                 Image sprite) {
-        super(position, name, sprite);
-        init(position, isDynamic);
-    }
-    
-    // TODO this will not be needed in the end, polygon is rendered
-    // for debugging. Graphics parameter migh be redundant.
-//    @Override
-//    public void render(Graphics graphics) {
-//        graphics.draw(polygon);
-//    }
 
-    private void init(Vec2 position, boolean isDynamic) {
-        
-        /***********
-         * PHYSICS *
-         ***********/
-        
-        // Body definition
-        bodyDef = new BodyDef();        
-        bodyDef.type = isDynamic == true? BodyType.DYNAMIC : BodyType.STATIC;
-		
+    private BodyDef bodyDef;	
+    private FixtureDef fixtureDef; 	
+	private Shape box2dShape;
+
+    public AbstractPhysicsEntity(final Vec2 position, 
+                                 final Vec2 size,
+								 final boolean isDynamic,
+								 final BodyShape bodyShape,
+                                 final String name) {        
+        super(position, size, bodyShape, name);
+		init(position, isDynamic, bodyShape);
+		this.bodyShape = bodyShape;
+    }
+
+    public AbstractPhysicsEntity(final Vec2 position,                                 
+								 final Image sprite,
+								 final boolean isDynamic,
+								 final BodyShape bodyShape,
+                                 final String name) {
+        super(position, bodyShape, sprite, name);
+		init(position, isDynamic, bodyShape);
+		this.bodyShape = bodyShape;
+    }
+
+	public AbstractPhysicsEntity(String name) {
+		super(name);
+	}
+	
+	protected void initBody(boolean isDynamic, Vec2 position) {		
+        bodyDef = new BodyDef();
 		if (name.equals("HERO")) {
-			System.out.println("init phys: " + (position.y + size.y/2));
-		}
+			bodyDef.fixedRotation = true;
+		} 
+        bodyDef.type = isDynamic == true? BodyType.DYNAMIC : BodyType.STATIC;
 		
         bodyDef.position.set((position.x + size.x/2)/PTM_SCALE, 
                              (position.y + size.y/2)/PTM_SCALE);
         body = GameplayState.getWorld().createBody(bodyDef);
 		body.m_userData = this;
-
-        // Shape
-        box = new PolygonShape();
-        box.setAsBox((size.x/2)/PTM_SCALE, 
-                     (size.y/2)/PTM_SCALE);
+	}
+	
+	protected void addDefaultFixture() {
+		// Shape
+        box2dShape = new PolygonShape();
+        ((PolygonShape)box2dShape).setAsBox((size.x/2)/PTM_SCALE, 
+					                 (size.y/2)/PTM_SCALE);
 
         // Fixture
         fixtureDef = new FixtureDef();
-        fixtureDef.shape = box;
-//        fixtureDef.density = 0.0f;
-//        fixtureDef.friction = 0.5f;
-//        fixtureDef.restitution = 0.0f;
-        
+        fixtureDef.shape = box2dShape;
+        fixtureDef.density = 0f;
+		fixtureDef.friction = 0.5f;
+        fixtureDef.restitution = 0.0f;
+        						
         body.createFixture(fixtureDef);
-
-		
-        /************
-         * Graphics *
-         ************/                
-        
-        // Initialise position
-        polygon.setX((body.getPosition().x*PTM_SCALE - size.x/2));
-        polygon.setY(GameplayState.getContainer().getHeight() -
-                            (body.getPosition().y*PTM_SCALE + size.y/2));
-		if (this.getName().equals("HERO")) {
-//			System.out.println("y:" + (
-//                            (body.getPosition().y*PTM_SCALE -  size.y/2)));
-			System.out.println("init pol: " + polygon.getY());
-		}
-    }
+	}
+	
+	protected void updatePosition() {
+		slick2dShape.setX((body.getPosition().x*PTM_SCALE - size.x/2));
+        slick2dShape.setY(GameplayState.getContainer().getHeight() -
+                          body.getPosition().y*PTM_SCALE - size.y/2);
+	}
 	
 	public Body getBody() {
 		return body;
+	}
+
+	protected void init(final Vec2 position, 
+			            final boolean isDynamic, 
+		                final BodyShape bodyShape) {
+		
+		/***********
+         * PHYSICS *
+         ***********/
+        
+		initBody(isDynamic, position);
+
+		// Available vertices
+		/*
+		 * D_________C
+		 * |        |
+		 * |        |
+		 * |        |
+		 * ----------
+		 * A		B
+		 *
+		 */
+
+		box2dShape = new PolygonShape();		
+		switch (bodyShape) {
+			case TRIANGLE_0 : {
+				Vec2[] triangle = new Vec2[] {new Vec2(coords[0]/PTM_SCALE, 
+													   coords[1]/PTM_SCALE),
+						                      new Vec2(coords[2]/PTM_SCALE,
+						                               coords[3]/PTM_SCALE),
+									          new Vec2(coords[4]/PTM_SCALE,
+						                               coords[5]/PTM_SCALE)};
+				((PolygonShape)box2dShape).set(triangle, 3);
+				break;
+			}
+			case TRIANGLE_1 : {
+				Vec2[] triangle = new Vec2[] {new Vec2(coords[0]/PTM_SCALE,
+						                               coords[1]/PTM_SCALE),
+									          new Vec2(coords[2]/PTM_SCALE,
+						                               coords[3]/PTM_SCALE),
+									          new Vec2(coords[6]/PTM_SCALE,
+						                               coords[7]/PTM_SCALE)};
+				((PolygonShape)box2dShape).set(triangle, 3);
+				break;
+			}
+			case SQUARE : {
+				box2dShape = new PolygonShape();
+				((PolygonShape)box2dShape).setAsBox((size.x/2)/PTM_SCALE, 
+ 					    	                        (size.y/2)/PTM_SCALE);
+				break;
+			}
+			case CIRCLE : {
+				box2dShape = new CircleShape();
+				box2dShape.m_radius = (size.y/2)/PTM_SCALE;
+				break;
+			}
+		}        										
+
+        // Fixture
+        fixtureDef = new FixtureDef();
+        fixtureDef.shape = box2dShape;
+        fixtureDef.density = 1.0f;
+		fixtureDef.friction = 0.5f;
+		fixtureDef.restitution = 0.0f;
+//		fixtureDef.filter.categoryBits = 0x0001;
+		fixtureDef.filter.categoryBits = CollisionFilter.TILE_BITS.getFilter();
+//		fixtureDef.filter.groupIndex = -1;
+		fixtureDef.filter.maskBits = CollisionFilter.ALL.getFilter();
+//		fixtureDef.filter.maskBits = CollisionFilter.ALL.getFilter();
+		if (name.contains("BULLET")) {
+			fixtureDef.restitution = 0.7f;
+			fixtureDef.friction = 0.0f;
+		}
+        
+		if (name.contains("DUMMY")) {
+			fixtureDef.restitution = 0.0f;
+			fixtureDef.friction = 0.0f;
+			fixtureDef.density = 0.0f;
+            bodyDef.fixedRotation = true;
+		}
+		
+		if (name.contains("SPAWNED")) {
+			fixtureDef.restitution = 0.0f;
+			fixtureDef.friction = 0.8f;
+			fixtureDef.density = 100.0f;
+			bodyDef.fixedRotation = true;
+		}
+		
+		if (name.contains("MOVING_TILE")) {
+			fixtureDef.restitution = 0.0f;
+			fixtureDef.friction = 0.8f;
+			fixtureDef.density = 1.0f;
+//			bodyDef.fixedRotation = true;
+            fixtureDef.isSensor = true;
+		}
+		
+        
+        
+		if (name.contains("SENSOR")) {		
+			fixtureDef.isSensor = true;
+//			fixtureDef.filter.maskBits = CollisionFilter.TILE_BITS.getFilter();
+		}
+		
+		if (name.equals("HERO")) {
+			fixtureDef.friction = 0.3f;
+			fixtureDef.density = 0.0f;
+//			fixtureDef.restitution = 10.0f;
+			bodyDef.fixedRotation = true;
+//			bodyDef.linearDamping = 2.1f;
+//			fixtureDef.filter.categoryBits = 0x0003;
+			
+//			fixtureDef.filter.categoryBits = CollisionFilter.HERO_BITS.getFilter();
+//			fixtureDef.filter.maskBits = CollisionFilter.ALL.getFilter();
+		}				
+		
+        body.createFixture(fixtureDef);		
+		
+//		if (name.equals("HERO")) {
+//			setFixtureDef(new FilterData(CollisionFilter.HERO_BITS, 
+//									     CollisionFilter.ALL));
+//		}
+		
+        /************
+         * Graphics *
+         ************/
+        
+        // Initialise position
+        updatePosition();
+	}
+	
+	public BodyShape getBodyShape() {
+		return bodyShape;
+	}
+	
+	public Shape getBox2DShape() {
+		return box2dShape;
+	}
+	
+	public void setFixtureDef(FilterData filterData) {
+		Filter filter = new Filter();
+		filter.categoryBits = filterData.categoryBits.getFilter();
+		filter.maskBits = filterData.maskBits.getFilter();
+		
+		for (Fixture fixture = body.getFixtureList();
+			 fixture != null; 
+			 fixture = fixture.getNext()) {
+			fixture.setFilterData(filter);
+		}
+		
 	}
 }
